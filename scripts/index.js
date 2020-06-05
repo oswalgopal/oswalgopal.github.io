d59b4d8d0ce7fda2dfac1e57d7332797b3a92b0f
oswalgopal <gploswal@gmail.com>
2020-06-05 16:18:59 +0530

    added contact form

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,28 @@
+const contactFormObj = {
+    name: '',
+    email: '',
+    purpose: ''
+};
 var myTerminal = new Terminal(10);
 terminal.appendChild(myTerminal.html);
 myTerminal.setTextSize('20px');
 myTerminal.setTextColor('#00FF00');
-myTerminal.blinkingCursor(false);
+myTerminal.blinkingCursor(true);
 myTerminal.setHeight(window.innerHeight);
 const date = new Date();
-myTerminal.print("Welcome to the world of the terminal");
-myTerminal.print('Your session started at ' + date.toLocaleDateString() + ' ' + date.toLocaleTimeString());
-myTerminal.print('Enter help to list the available comands');
+myTerminal.print('==> Your session started at ' + date.toDateString() + ' ' + date.toLocaleTimeString());
+myTerminal.print("==> Welcome to the terminal of Gopal Oswal! :)");
+myTerminal.print("==> This terminal will give all info about me.");
+myTerminal.print("==> Hope You like it :)");
+myTerminal.print('--------------------------------------------------------')
+myTerminal.print('Enter `help` to list the available commands');
 myTerminal.input('', command => {
         handleInput(command)
 });
 const handleInput  = command => {
     if (command.length) {
-        if (command === 'help' || command === 'ls') {
+        command = command.toLowerCase();
+        if (command === 'help' || command === 'ls'|| command === 'man') {
             handleHelp();
         } else if (command === 'about'){
             showAbout();
@@ -29,28 +38,30 @@ const handleInput  = command => {
             showSkills();
         } else if (command === 'contact'){
             showContact();
-        } else if (command === 'close') {
-            // window.close();
+        } else if (command === 'exit') {
+            myTerminal.print('> Thank You for visiting my website thanks:)')
         } else if (command === 'clear') {
             myTerminal.clear();
         } else {
             handleError(command);
         }
+    } else {
+        myTerminal.beep();
     }
-    if (command !== 'exit') {
+    if (command !== 'exit' && command !== 'contact') {
         myTerminal.input('', command2 => {
             handleInput(command2);
         });
     }
 }
 const showAbout = () => {
-    myTerminal.print('I am Gopal Oswal');
-    myTerminal.print('Full Stack | MEAN STack | MERN STACK  web developer.');
-    myTerminal.print("According to me, The only source of knowledge is experience ");
-    myTerminal.print("I am a Full Stack Developer, working with Angular, Ionic, React, React Native, Flutter, Posgresql, Nodejs etc.");
-    myTerminal.print("I also know version control system like Git and also experienced in both Github and Gitlab.");
-    myTerminal.print("I love to meet people and interact with them to explore my knowledge.");
-    myTerminal.print("I am specialized in developing web and mobile applications for your business and personal requirements.");
+    myTerminal.print('==> I am Gopal Oswal');
+    myTerminal.print('==> Full Stack | MEAN Stack | MERN STACK  web developer.');
+    myTerminal.print("==> According to me, The only source of knowledge is experience ");
+    myTerminal.print("==> I am a Full Stack Developer, working with Angular, Ionic, React, React Native, Flutter, Posgresql, Nodejs etc.");
+    myTerminal.print("==> I also know version control system like Git and also experienced in both Github and Gitlab.");
+    myTerminal.print("==> I love to meet people and interact with them to explore my knowledge.");
+    myTerminal.print("==> I am specialized in developing web and mobile applications for your business and personal requirements.");
 }
 
 const showSkills = () => {
@@ -83,22 +94,53 @@ const showContact = () => {
     myTerminal.print('> gploswal@gmail.com');
     myTerminal.print('> https://github.com/oswalgopal');
     myTerminal.print('> https://linkedin.com/in/oswalgopal');
+    myTerminal.input('Want to the Contact form [y/n]', confirm => {
+        handleContactForm(confirm);
+    })
+}
+
+
+const handleContactForm = confirm => {
+    if (confirm.toLowerCase() === 'y') {
+        myTerminal.input('Enter your name ', name => {
+            contactFormObj.name = name;
+            myTerminal.input('Email Id', email => {
+                contactFormObj.email = email;
+                myTerminal.input('Purpose of contact', purpose => {
+                    contactFormObj.purpose = purpose;
+                    myTerminal.print('----------------------------------------------------------');
+                    myTerminal.print('Thanks for filling the form you will get in contact soon:)');
+                    myTerminal.input('', command2 => {
+                        handleInput(command2);
+                    });
+                });
+            });
+        });
+    } else if (confirm.toLowerCase() === 'n') {
+        myTerminal.input('', command2 => {
+            handleInput(command2);
+        });
+    } else {
+        myTerminal.input('Invalid selection Please select a valid option', confirm => {
+            handleContactForm(confirm)
+        });
+    }
 }
 
 const handleHelp = () => {
-    myTerminal.print('Hey, Thanks for using Terminal :)');
-    myTerminal.print('The available command in my terminal are :');
+    myTerminal.print('==> Hey, Thanks for using Terminal :)');
+    myTerminal.print('==> The available command in my terminal are :');
     const commands = [
         {name: 'about' },
         {name: 'skills'},
         {name: 'work'},
         {name: 'education'},
         {name: 'achievements'},
-        {name: 'skills'},
         {name: 'projects'},
         {name: 'contact'},
         {name: 'clear'},
         {name: 'help'},
+        {name: 'exit'},
     ]
     commands.forEach((element) => {
         myTerminal.print(` > ${element.name}`);
@@ -106,7 +148,7 @@ const handleHelp = () => {
 }
 
 const handleError = (command) => {
-    myTerminal.print('"' + command + '" is not recognised as a valid command ');
+    myTerminal.print('`' + command + '` is not recognised as a valid command ');
     myTerminal.print('use help to know more about commands');
 }
 
