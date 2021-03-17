0e5be8817d4e9ede3831683ab9036f4828782eac
oswalgopal <gploswal@gmail.com>
2021-03-17 18:03:59 +0530

    added image

---
+++ img/paths.js
@@ -0,0 +1,21 @@
+const path = {
+    ReactNative: require('../../img/skills/react native.png'),
+    ReactJs: require('../../img/skills/react.png'),
+    Angular: require('../../img/skills/angular.png'),
+    Ionic: require('../../img/skills/ionic.png'),
+    Java: require('../../img/skills/java.png'),
+    Python: require('../../img/skills/python.png'),
+    Flask: require('../../img/skills/flask.png'),
+    Django: require('../../img/skills/django.png'),
+    Sql: require('../../img/skills/sql.png'),
+    Node: require('../../img/skills/node.png'),
+    mongodDb: require('../../img/skills/mongodb.png'),
+    PS: require('../../img/skills/PS.png'),
+    AI: require('../../img/skills/AI.png'),
+    C: require('../../img/skills/cpp.png'),
+    GIT: require('../../img/skills/git.png'),
+    ReactJs: require('../../img/skills/6.png'),
+    ReactJs: require('../../img/skills/6.png'),
+}
+
+module.export = path;
